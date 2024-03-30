c09868da2bca1ffd2aabf41a73762d98c8d9ecf2
aronbarbosag <aronfbarbosa@gmail.com>
2024-03-29 23:43:18 -0300

    teste

---
+++ script.js
@@ -0,0 +1,64 @@
+const timer = document.querySelector('.card-title');
+const btnIniciar = document.getElementById('iniciar');
+const btnPausar = document.getElementById('pausar');
+const btnZerar = document.getElementById('zerar');
+let inicioTimer;
+const horario = {
+  horas: 0,
+  minutos: 0,
+  segundos: 0,
+};
+function zeroEsquerda(num) {
+  return num >= 10 ? num : `0${num}`;
+}
+
+function desabilitarBtn() {
+  btnIniciar.setAttribute('disabled', '');
+}
+function habilitarBtn() {
+  btnIniciar.removeAttribute('disabled');
+}
+
+function iniciarTimer(event) {
+  event.preventDefault();
+
+  desabilitarBtn();
+  inicioTimer = setInterval(() => {
+    timer.innerHTML = `${zeroEsquerda(horario.horas)}:${zeroEsquerda(
+      horario.minutos
+    )}:${zeroEsquerda(horario.segundos)}`;
+    horario.segundos += 1;
+    if (horario.segundos === 60) {
+      horario.segundos = 0;
+      horario.segundos += 1;
+    }
+    if (horario.minutos === 60) {
+      horario.minutos = 0;
+      horario.horas += 1;
+    }
+  }, 1000);
+}
+
+function pausarTimer(event) {
+  event.preventDefault();
+
+  clearInterval(inicioTimer);
+  habilitarBtn();
+}
+
+function zerarTimer(event) {
+  event.preventDefault();
+  habilitarBtn();
+
+  horario.segundos = 0;
+  horario.minutos = 0;
+  horario.horas = 0;
+  timer.innerHTML = `${zeroEsquerda(horario.horas)}:${zeroEsquerda(
+    horario.minutos
+  )}:${zeroEsquerda(horario.segundos)}`;
+  pausarTimer();
+}
+
+btnIniciar.addEventListener('click', iniciarTimer);
+btnPausar.addEventListener('click', pausarTimer);
+btnZerar.addEventListener('click', zerarTimer);
